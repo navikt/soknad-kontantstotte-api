b2290263bfc0c4fcb8bef9180aa076a544f44a75
Martine Enger <marenger@student.matnat.uio.no>
2018-06-07 20:37:07 +0200

    [kaizen] [wip] bruker versjon 2.26 av jersey

--- src/test/java/no/nav/kontantstotte/api/rest/StatusResourceTest.java
+++ src/test/java/no/nav/kontantstotte/api/rest/StatusResourceTest.java
@@ -1,14 +1,10 @@
 package no.nav.kontantstotte.api.rest;
 
 import no.nav.kontantstotte.api.config.RestConfiguration;
-import no.nav.kontantstotte.api.security.OidcJerseyRequestFilter;
-import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Application;
 
 public class StatusResourceTest extends JerseyTest {
@@ -26,15 +22,14 @@ public class StatusResourceTest extends JerseyTest {
 
     @Override
     protected Application configure() {
-        return new RestConfiguration()
-                .register(StatusResource.class)
-                .register(OidcJerseyRequestFilter.class);
+        return new RestConfiguration();
     }
 
     @Test
-    public void test() {
-        WebTarget target = target();
+    public void testStatus() throws Exception {
+        setUp();
 
+        target("/status/ping");
     }
 
 }
