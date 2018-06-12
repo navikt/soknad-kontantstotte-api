cbeefb49ef7886aaf9443e2134ca0aedcbd89b5d
Martine Enger <marenger@student.matnat.uio.no>
2018-06-12 13:49:17 +0200

    Sd-195: nais.yaml -> app.config.yaml

--- src/main/java/no/nav/kontantstotte/api/rest/StatusResource.java
+++ src/main/java/no/nav/kontantstotte/api/rest/StatusResource.java
@@ -1,6 +1,7 @@
 package no.nav.kontantstotte.api.rest;
 
 import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
+import no.nav.security.spring.oidc.validation.api.Unprotected;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -10,12 +11,17 @@ import javax.ws.rs.core.StreamingOutput;
 
 @Component
 @Path("status")
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
 public class StatusResource {
 
     @GET
     @Path("ping")
+    @ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
     public String ping() {
         return "pong";
     }
+
+    @GET
+    @Path("isAlive")
+    @Unprotected
+    public String isAlive() { return "Ok"; }
 }
