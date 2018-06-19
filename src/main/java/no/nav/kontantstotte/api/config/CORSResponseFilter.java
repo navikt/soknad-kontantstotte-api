14d340f542d8176fad55c545763fa2590ce590d9
Martine Enger <marenger@student.matnat.uio.no>
2018-06-19 08:55:22 +0200

    SD-365: Oppdatert filter til å kun tillate soknad-kontantstotte som origin

--- src/main/java/no/nav/kontantstotte/api/config/CORSResponseFilter.java
+++ src/main/java/no/nav/kontantstotte/api/config/CORSResponseFilter.java
@@ -1,23 +1,42 @@
 package no.nav.kontantstotte.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Provider
 public class CORSResponseFilter implements ContainerResponseFilter {
 
+    private List<String> allowedOriginsList;
+
+    @Inject
+    public CORSResponseFilter(@Value("${no.nav.kontantstotte.api.allowed.origins:" +
+            "https://soknad-kontantstotte.nais.oera-q.local," +
+            "https://soknad-kontantstotte-q.nav.no," +
+            "https://soknad-kontantstotte.nav.no," +
+            "}") String... allowedOrigins) {
+        allowedOriginsList = Arrays.asList(allowedOrigins);
+    }
+
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) {
 
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        String origin = request.getHeaderString("Origin");
+        if (allowedOriginsList.contains(origin)) {
+            response.getHeaders().add("Access-Control-Allow-Origin", origin);
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                    "origin, content-type, accept, authorization");
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        }
     }
 }
