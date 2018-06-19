a483d402883dfcc4cdbf3d0618674ff829310195
Martine Enger <marenger@student.matnat.uio.no>
2018-06-19 09:38:45 +0200

    [kaizen] Jersey likte ikke konstruktør så la allowed origins i konstant

--- src/main/java/no/nav/kontantstotte/api/config/CORSResponseFilter.java
+++ src/main/java/no/nav/kontantstotte/api/config/CORSResponseFilter.java
@@ -1,36 +1,26 @@
 package no.nav.kontantstotte.api.config;
 
-import org.springframework.beans.factory.annotation.Value;
-
-import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
-import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 
 @Provider
 public class CORSResponseFilter implements ContainerResponseFilter {
 
-    private List<String> allowedOriginsList;
-
-    @Inject
-    public CORSResponseFilter(@Value("${no.nav.kontantstotte.api.allowed.origins:" +
-            "https://soknad-kontantstotte.nais.oera-q.local," +
-            "https://soknad-kontantstotte-q.nav.no," +
-            "https://soknad-kontantstotte.nav.no," +
-            "}") String... allowedOrigins) {
-        allowedOriginsList = Arrays.asList(allowedOrigins);
-    }
-
+    private static final List<String> ALLOWED_ORIGINS = asList("https://soknad-kontantstotte.nais.oera-q.local",
+            "https://soknad-kontantstotte-q.nav.no",
+            "https://soknad-kontantstotte.nav.no");
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) {
 
         String origin = request.getHeaderString("Origin");
-        if (allowedOriginsList.contains(origin)) {
+        if (ALLOWED_ORIGINS.contains(origin)) {
             response.getHeaders().add("Access-Control-Allow-Origin", origin);
             response.getHeaders().add("Access-Control-Allow-Headers",
                     "origin, content-type, accept, authorization");
