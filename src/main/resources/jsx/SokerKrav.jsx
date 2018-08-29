1d2e0160d7da545e8d4dcea37adbd1cae2638f11
Henning Håkonsen <henninghaakonsen@me.com>
2018-08-29 16:07:43 +0200

    Lagt på tekster, samt oppdatert pdf generatoren slik at den tar med alle punktene.

--- src/main/resources/jsx/SokerKrav.jsx
+++ src/main/resources/jsx/SokerKrav.jsx
@@ -5,13 +5,22 @@ var SokerKrav = React.createClass({
                 <h3>Kravene av elektronisk søknad</h3>
                 <ul>
                     <OppsummeringsListeElement
-                        tekst={'Jeg har bodd eller vært yrkesaktiv i Norge i minst fem år'}
+                        tekst={'Jeg er statsborger i Norge eller i et annet EØS-land'}
                     />
                     <OppsummeringsListeElement
-                        tekst={'Jeg bor sammen med barnet'}
+                        tekst={'Jeg har bodd eller vært yrkesaktiv i Norge eller et annet EØS-land i tilsammen minst fem år'}
                     />
                     <OppsummeringsListeElement
-                        tekst={'Jeg og barnet skal bo i Norge de neste 12 månedene'}
+                        tekst={'Barnet er ikke adoptert, asylsøker i fosterhjem eller på institusjon'}
+                    />
+                    <OppsummeringsListeElement
+                        tekst={'Jeg bor i Norge sammen med barnet'}
+                    />
+                    <OppsummeringsListeElement
+                        tekst={'Jeg og den andre forelderen har ikke avtalt delt bosted'}
+                    />
+                    <OppsummeringsListeElement
+                        tekst={'Jeg og barnet skal ikke oppholde dere i utlandet mer enn 3 måneder de neste 12 månedene'}
                     />
                 </ul>
             </div>
