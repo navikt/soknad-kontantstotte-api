c48e772f5b6c3ea89788e9f206f8afff7362d13d
Martine Enger <marenger@student.matnat.uio.no>
2018-09-03 13:58:54 +0200

    Fjernet flyttet/duplisert mappe

--- src/main/react-pdf/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOMServer from 'react-dom/server';
-import SoknadPdf from './SoknadPdf';
-
-const renderApp = (soknad) => {
-    ReactDOMServer.renderToStaticMarkup(
-        React.render(<SoknadPdf soknad={soknad} />, document.getElementById('app'))
-    )
-};
