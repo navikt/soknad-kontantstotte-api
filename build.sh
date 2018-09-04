3c83af6dfe70a876d5eb241e7717e0e3c03d494c
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-09-04 14:07:34 +0200

    [kaizen] Rydder opp i filer som ikke er i bruk

--- build.sh
+++ build.sh
@@ -59,6 +59,7 @@ function build_command {
 function build_pdf_template {
     CURRENT_DIR=$(pwd)
     cd src/main/resources/react-pdf/
+    mkdir dist
     build_command npm install
     build_command npx babel src --out-file dist/bundle.js --presets=@babel/preset-env,@babel/preset-react
     cd $CURRENT_DIR
--- src/main/resources/react-pdf/src/index.js
+++
@@ -1,5 +0,0 @@
-const renderApp = (soknad) => {
-    ReactDOMServer.renderToStaticMarkup(
-        React.render(<SoknadPdf soknad={soknad} />, document.getElementById('app'))
-    )
-};
