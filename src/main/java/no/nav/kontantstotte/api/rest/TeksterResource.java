94ea2bc19b3468a9d9f0a906a66e18ab7c7a1da9
Einar Bjerve <einar.bjerve@nav.no>
2018-09-07 08:59:39 +0200

    Nynorsk-støtte

--- src/main/java/no/nav/kontantstotte/api/rest/TeksterResource.java
+++ src/main/java/no/nav/kontantstotte/api/rest/TeksterResource.java
@@ -5,12 +5,19 @@ import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.Properties;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.ResourceBundle.getBundle;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 @Component
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,21 +25,25 @@ import java.util.Properties;
 @Unprotected
 public class TeksterResource {
 
-    private static final String TEKSTER_FILENAME = "tekster.properties";
-    private final Properties properties;
+    private static final String BUNDLE_NAME = "tekster";
+
+    private static final String[] VALID_LANGUAGES = { "nb", "nn" };
+
+    private final Map<String, Map<String, String>> properties;
 
     public TeksterResource() {
-        properties = new Properties();
-        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(TEKSTER_FILENAME);
-        try {
-            properties.load(new InputStreamReader(resourceAsStream, Charset.forName("UTF-8")));
-        } catch (Exception e) {
-            throw new RuntimeException(String.format("Feil ved lesing av tekster. Finnes filen %s i resources?", TEKSTER_FILENAME));
-        }
+
+        Function<ResourceBundle, Map<String, String>> bundleToMap = bundle -> bundle.keySet().stream()
+                .collect(toMap(identity(), bundle::getString));
+
+        properties = Arrays.stream(VALID_LANGUAGES)
+                .map(language -> getBundle(BUNDLE_NAME, new Locale(language)))
+                .collect(toMap(bundle -> bundle.getLocale().getLanguage(), bundleToMap));
     }
 
     @GET
-    public Properties tekster() {
-        return properties;
+    @Path("{language}")
+    public Map<String, String> tekster(@PathParam("language") String language) {
+        return properties.get(language);
     }
 }
