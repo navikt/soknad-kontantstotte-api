b7471e227d8b35490505cbd965ae32ba121e6f88
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-09-07 10:31:57 +0200

    [SD-481] Refaktorerer funksjonen som henter htmlstringen for PDF, til å være litt mer oversiktelig

--- src/main/resources/react-pdf/src/SoknadPdf.jsx
+++ src/main/resources/react-pdf/src/SoknadPdf.jsx
@@ -16,15 +16,21 @@ class SoknadPdf extends React.Component {
 
 function hentHTMLStringForOppsummering(soknad) {
     var komponent = ReactDOMServer.renderToStaticMarkup(<SoknadPdf soknad={soknad} />);
-    return '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"/>' +
-            hentStyleHeader() +
-        '</head><body>' + komponent + '</body></html>';
-}
+    const template = `
+        <html>
+            <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+            <style type="text/css"> 
+                @page:first { margin: 12mm 2cm 2.5cm; } 
+                *       { box-sizing: border-box; } 
+                body    { font-family: ArialSystem, sans-serif; font-size: 10pt; line-height: 1.4em; margin: 0; color: #3e3832; } 
+            </style>
+            </head>
+            <body>
+            ${komponent}
+            </body>
+        </html>
+`;
 
-function hentStyleHeader() {
-    return '<style type="text/css"> \
-        @page:first { margin: 12mm 2cm 2.5cm; } \
-        *       { box-sizing: border-box; } \
-        body    { font-family: ArialSystem, sans-serif; font-size: 10pt; line-height: 1.4em; margin: 0; color: #3e3832; } \
-    </style>';
+    return template
 }
