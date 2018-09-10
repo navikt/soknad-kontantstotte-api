24ede4a2b35a1464c3858a81600aa2bb6539e24e
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-09-10 08:29:56 +0200

    [SD-464] Legger til styling på tittelkomponent (demo/guide på hvordan style komponentene videre).

--- src/main/resources/react-pdf/src/SoknadPdf.jsx
+++ src/main/resources/react-pdf/src/SoknadPdf.jsx
@@ -1,7 +1,23 @@
+const wrapper = {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column'
+};
+
+const SideTittel = {
+    backgroundColor:'#c1b5d0',
+    margin:'20px 0 40px 0',
+    padding: '20px 0 20px 0',
+    textAlign: 'center'
+};
+
 const SoknadPdf = (props) => {
     return (
-        <div>
-            <h1>Søknad om kontantstøtte</h1>
+        <div style={wrapper}>
+            <div style={SideTittel}>
+                <h1>Søknad om kontantstøtte</h1>
+            </div>
+
             <Personalia person={props.soknad.person} />
             <SokerKrav kravTilSoker={props.soknad.kravTilSoker}/>
             <Barn mineBarn={props.soknad.mineBarn}/>
@@ -19,7 +35,7 @@ function hentHTMLStringForOppsummering(soknad) {
             <head>
             <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
             <style type="text/css"> 
-                @page:first { margin: 12mm 2cm 2.5cm; } 
+                @page   { margin: 12mm 1cm 2.5cm; } 
                 *       { box-sizing: border-box; } 
                 body    { font-family: ArialSystem, sans-serif; font-size: 10pt; line-height: 1.4em; margin: 0; color: #3e3832; } 
             </style>
