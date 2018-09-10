53c7e5de48749adb3c82a8a5a4793af751b19456
Martine Enger <marenger@student.matnat.uio.no>
2018-09-10 15:12:13 +0200

    SD-310: Lagt til nye tekster. Fjerner ubrukte felter fra domenemodell + pdf

--- src/main/resources/jsx/Familieforhold.jsx
+++ src/main/resources/jsx/Familieforhold.jsx
@@ -4,7 +4,6 @@ var Familieforhold = React.createClass({
             <div>
                 <h3>Opplysning om familieforhold:</h3>
                 <ul>
-                    <li>Har foreldrene delt bosted: {this.props.familieforhold.borForeldreneSammenMedBarnet}</li>
                     {this.props.familieforhold.borForeldreneSammenMedBarnet === 'JA' &&
                         <OppsummeringsListeElement
                             tekst={'Jeg bor sammen med den andre forelderen'}
@@ -14,6 +13,12 @@ var Familieforhold = React.createClass({
                             <h4>Fodselsnummeret til den andre forelderen:</h4>
                             <p>{this.props.familieforhold.annenForelderFodselsnummer}</p>
                         </OppsummeringsListeElement>
+                    }
+
+                    {this.props.familieforhold.borForeldreneSammenMedBarnet === 'NEI' &&
+                    <OppsummeringsListeElement
+                        tekst={'Jeg bor ikke sammen med den andre forelderen'}
+                    />
                     }
                 </ul>
             </div>
