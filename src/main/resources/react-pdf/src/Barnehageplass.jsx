5180da43c6e8fe4c10946fa9fe18cc1d2cc8553f
Henning Håkonsen <henninghaakonsen@me.com>
2018-09-13 13:54:07 +0200

    Endret paa logikk for a hente ut tekst nokkel.

--- src/main/resources/react-pdf/src/Barnehageplass.jsx
+++ src/main/resources/react-pdf/src/Barnehageplass.jsx
@@ -2,6 +2,26 @@ const Barnehageplass = (props) => {
     const date = new Date(props.barnehageplass.dato);
     const dateString = date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
 
+    let barnBarnehageplassStatusKey = 'Ubesvart';
+
+    switch (props.barnehageplass.barnBarnehageplassStatus) {
+        case 'garIkkeIBarnehage':
+            barnBarnehageplassStatusKey = 'barnehageplass.garIkkeIBarnehage';
+            break;
+        case 'harBarnehageplass':
+            barnBarnehageplassStatusKey = 'barnehageplass.harBarnehageplass';
+            break;
+        case 'harSluttetIBarnehage':
+            barnBarnehageplassStatusKey = 'barnehageplass.harSluttetIBarnehage';
+            break;
+        case 'skalBegynneIBarnehage':
+            barnBarnehageplassStatusKey = 'barnehageplass.skalBegynneIBarnehage';
+            break;
+        case 'skalSlutteIBarnehage':
+            barnBarnehageplassStatusKey = 'barnehageplass.skalSlutteIBarnehage';
+            break;
+    }
+
     return (
         <Bolk>
             <h3 style={Uppercase}>{props.tekster['oppsummering.barnehageplass.tittel']}</h3>
@@ -12,7 +32,7 @@ const Barnehageplass = (props) => {
 
             <OppsummeringsElement
                 sporsmal={props.tekster['barnehageplass.barnBarnehageplassStatus']}
-                svar={props.tekster['barnehageplass.'.concat(props.barnehageplass.barnBarnehageplassStatus)]}
+                svar={props.tekster[barnBarnehageplassStatusKey]}
             />
         </Bolk>
     )
