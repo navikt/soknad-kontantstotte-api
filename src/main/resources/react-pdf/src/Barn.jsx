e8db655f8e86c13598de0bb2f62119521781358d
Henning Håkonsen <henninghaakonsen@me.com>
2018-09-19 12:02:58 +0200

    Mindre tekstendringer

--- src/main/resources/react-pdf/src/Barn.jsx
+++ src/main/resources/react-pdf/src/Barn.jsx
@@ -2,7 +2,7 @@ const Barn = (props) => {
         return (
             <Bolk>
                 <h3 style={Uppercase}>{props.tekster['barn.tittel']}</h3>
-                <h3>{props.tekster['barn.pdf.subtittel']}</h3>
+                <h3>{props.tekster['oppsummering.barn.subtittel']}</h3>
                 <OppsummeringsElement
                     sporsmal={props.tekster['barn.navn']}
                     svar={props.mineBarn.navn}
