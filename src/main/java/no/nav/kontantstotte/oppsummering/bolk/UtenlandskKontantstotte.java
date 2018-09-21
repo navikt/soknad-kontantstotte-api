c552fbc967af241eeddb02447dfc30c499be9cd4
Ørjan Helstrøm <orjan.breimo.helstrom@nav.no>
2018-09-21 12:50:29 +0200

    SD-543-bekreftelse * Fikser feil etter merge med master

--- src/main/java/no/nav/kontantstotte/oppsummering/bolk/UtenlandskKontantstotte.java
+++ src/main/java/no/nav/kontantstotte/oppsummering/bolk/UtenlandskKontantstotte.java
@@ -1,4 +1,4 @@
-package no.nav.kontantstotte.oppsummering;
+package no.nav.kontantstotte.oppsummering.bolk;
 
 public class UtenlandskKontantstotte {
     public String mottarKontantstotteFraUtlandet; 
