213f4260452ea2efc945dadd710ea77764bb7460
Henning Håkonsen <henninghaakonsen@me.com>
2018-09-25 09:34:51 +0200

    La til test mot pdfgen

--- src/main/java/no/nav/kontantstotte/service/PdfService.java
+++ src/main/java/no/nav/kontantstotte/service/PdfService.java
@@ -1,5 +1,9 @@
 package no.nav.kontantstotte.service;
 
+import no.nav.kontantstotte.api.rest.InnsendingResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
@@ -9,19 +13,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class PdfService {
+    private final Logger logger = LoggerFactory.getLogger(PdfService.class);
 
     private URI pdfGeneratorServiceUri;
+    private URI pdfgenServiceUri;
 
     private final Client client;
 
     public PdfService(Client client, URI pdfGeneratorServiceUri) {
         this.client = client;
         this.pdfGeneratorServiceUri = pdfGeneratorServiceUri;
+        try {
+            this.pdfgenServiceUri = new URI("http://pdf-gen/api");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     public byte[] genererPdf(String oppsummeringHtml) {
+        logger.warn(pdfGeneratorServiceUri + ", " + pdfgenServiceUri);
+        logger.warn(oppsummeringHtml.substring(0, 20));
 
         Response response = client
                 .target(pdfGeneratorServiceUri)
@@ -29,6 +43,15 @@ public class PdfService {
                 .request()
                 .buildPost(Entity.entity(oppsummeringHtml, MediaType.TEXT_HTML))
                 .invoke();
+
+        Response responseTwo = client
+                .target(pdfgenServiceUri)
+                .path("v1/genpdf/html/kontantstotte")
+                .request()
+                .buildPost(Entity.entity(oppsummeringHtml, "text/html; charset=utf-8"))
+                .invoke();
+
+        logger.warn(response.toString() + ", " + responseTwo.toString());
 
         return response.readEntity(byte[].class);
 
