bc8e6fd34723f44ef9f4d0ab4608507a4cc91d9d
Tine Kleivane <kleivane@gmail.com>
2018-10-05 12:15:10 +0200

    Legg til en default tekstprovider

---
+++ src/main/java/no/nav/kontantstotte/tekst/DefaultTekstProvider.java
@@ -0,0 +1,14 @@
+package no.nav.kontantstotte.tekst;
+
+public class DefaultTekstProvider extends TekstProvider {
+
+    private static final String DEFAULT_BUNDLE_NAME = "tekster";
+
+    private static final String[] DEFAULT_VALID_LANGUAGES = { "nb", "nn" };
+
+
+    public DefaultTekstProvider(){
+        super(DEFAULT_BUNDLE_NAME, DEFAULT_VALID_LANGUAGES);
+    }
+
+}
