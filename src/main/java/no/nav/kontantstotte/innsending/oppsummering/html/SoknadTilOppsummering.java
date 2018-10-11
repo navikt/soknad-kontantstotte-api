6f2036bc2c55847fe4a3545bfd1d881b55fbf981
Tine Kleivane <kleivane@gmail.com>
2018-10-11 17:12:52 +0200

    Oppdater kommentar

--- src/main/java/no/nav/kontantstotte/innsending/oppsummering/html/SoknadTilOppsummering.java
+++ src/main/java/no/nav/kontantstotte/innsending/oppsummering/html/SoknadTilOppsummering.java
@@ -16,12 +16,20 @@ import java.util.Map;
 import java.util.function.BiFunction;
 
 /**
- * Klassen benyttes til ny oppsummering generering.
- * mapBolker tar inn søknadsobjektet og mapper til ny
- * oppsummeringsobjekt som skal til ny html+oppsummering generator.
- * <p>
- * For hver steg man tar fra soknad til ny innsending skriver vi en map
- * funksjon som erstatter det gamle attributtet.
+ * Klassen benyttes til generering av oppsummeringsinnhold.
+ * mapBolker tar inn soeknadsobjektet og mapper til nytt
+ * oppsummeringsobjekt som skal til htmlconverter og deretter til pdfconverter.
+ *
+ * Maalet et er at SoknadOppsummering.java-objektet på sikt ikke skal inneholde soknad eller tekster,
+ * men kun være bolker med spoersmal og svar og eventuell metadata tilknyttet oppsummeringen
+ *
+ * Naar metoden nyBolk ikke er i bruk mer, kan vi slette soknadsobjektet fra SoknadOppsummering og deretter
+ * gjoere en del endringer i soknad-html-generator for å slette gamle react-filer og deretter ta grep for aa
+ * slette eventuelle gjenvaerende tekster som er i bruk.
+ *
+ * Et steg i soknaden kan byttes ut med ny generering ved aa erstatte kallet til nyBolk med en ny mapper-klasse.
+ * Husk aa endre i testene tilhoerende denne klassen og teste alle varianter i engen mapper
+ *
  */
 class SoknadTilOppsummering {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy - HH.mm")
