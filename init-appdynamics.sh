d030c9aa10b2aff9e02600ba559197d074723c98
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-29 13:43:10 +0100

    [kaizen] Fikser copy/paste feil

--- init-appdynamics.sh
+++ init-appdynamics.sh
@@ -3,8 +3,8 @@
 if test -r "/opt/appdynamics/agent.jar";
 then
     JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/agent.jar"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=my_app"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=soknad-kontantstotte-api"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=prod-${HOSTNAME}"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=prod-my_app"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=prod-soknad-kontantstotte-api"
     export JAVA_OPTS
 fi
