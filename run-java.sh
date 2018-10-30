201f38b992283133cd7f7d9c77ba502f366a2c04
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-30 11:57:28 +0100

    [SD-420] Oppdaterer run-java scriptet med riktige verdier.

--- run-java.sh
+++ run-java.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if test -r "/app/appdynamics/agent.jar";
 then
-    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/appdynamics/agent.jar"
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/appdynamics/javaagent.jar"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=soknad-kontantstotte-api"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=prod-${HOSTNAME}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=prod-soknad-kontantstotte-api"
     export JAVA_OPTS
 fi
