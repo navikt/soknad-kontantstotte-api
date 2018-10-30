815ce958e4191a0fae7f267c5297d5224c6b7490
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-30 13:26:05 +0100

    [SD-420] Oppdaterer app-dynamics skriptet

--- 10-app-dynamics.sh
+++ 10-app-dynamics.sh
@@ -2,10 +2,10 @@
 if test -r "/app/appdynamics/javaagent.jar";
 then
     JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/appdynamics/javaagent.jar"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=soknad-kontantstotte-api"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APP_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=prod-soknad-kontantstotte-api"
+    JAVA_OPTS="${JAVA_OPTS} -D-Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}"
     export JAVA_OPTS
 fi
