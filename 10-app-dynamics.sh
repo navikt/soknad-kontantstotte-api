57ff78471f4cf10386a896e58ac027070bb51c72
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-30 14:39:41 +0100

    [SD-420] Legger til nodename

--- 10-app-dynamics.sh
+++ 10-app-dynamics.sh
@@ -5,6 +5,7 @@ then
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APP_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=${HOSTNAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
     JAVA_OPTS="${JAVA_OPTS} -D-Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}"
     export JAVA_OPTS
