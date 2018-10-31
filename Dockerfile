14a2dfd7169456c0a4d6d11f543e44f9faa9f375
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-31 07:46:53 +0100

    [SD-420] Legger til config for appdynamics og tar i bruk ny base-image som har appdynamics agenten pre-installert.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM navikt/java:8
+FROM navikt/java:8-appdynamics
 
 ADD ./VERSION /app/VERSION
 COPY ./target/soknad-kontantstotte-api.jar "/app/app.jar"
+
+ADD ./appdynamics-config.sh /init-scripts
---
+++ appdynamics-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if test -r "/opt/appdynamics/javaagent.jar";
+then
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/javaagent.jar"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=soknad-kontantstotte-api"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}-soknad-kontantstotte-api"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    export JAVA_OPTS
+fi
