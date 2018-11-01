d998fea11107993b738d4d4b0eac9097cecc51df
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-11-01 13:39:34 +0100

    [SD-420] Tar i bruk innebygd appdynamics i baseimage, og refaktorerer configen.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
-FROM navikt/java:8
+FROM navikt/java:8-appdynamics
 
-RUN mkdir -p /app/appdynamics \
-    && wget -O /app/appdynamics/appdynamicsagent.zip http://maven.adeo.no/nexus/service/local/repositories/m2internal/content/com/appdynamics/appdynamics-appagent/4.5.1/appdynamics-appagent-4.5.1.zip \
-    && unzip /app/appdynamics/appdynamicsagent.zip -d /app/appdynamics
 COPY appdynamics.sh /init-scripts/
 
 ADD ./VERSION /app/VERSION
--- appdynamics.sh
+++ appdynamics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if test -r "/app/appdynamics/javaagent.jar";
+if test -r "/opt/appdynamics/javaagent.jar";
 then
-    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/appdynamics/javaagent.jar"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=soknad-kontantstotte-api"
-    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}-soknad-kontantstotte-api"
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/javaagent.jar"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APP_NAME}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}-${APP_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
