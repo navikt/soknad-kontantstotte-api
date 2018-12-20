e0077e637f260b04bc2e55cd2fd1b13986f65194
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2018-12-20 14:51:06 +0100

    SD-373: Legg til støtte for flerlinger

--- src/test/java/no/nav/kontantstotte/api/rest/BarnResourceTest.java
+++ src/test/java/no/nav/kontantstotte/api/rest/BarnResourceTest.java
@@ -3,7 +3,9 @@ package no.nav.kontantstotte.api.rest;
 import com.nimbusds.jwt.SignedJWT;
 import no.nav.kontantstotte.api.rest.dto.BarnDto;
 import no.nav.kontantstotte.config.ApplicationConfig;
-import no.nav.kontantstotte.innsyn.domain.*;
+import no.nav.kontantstotte.innsyn.domain.Barn;
+import no.nav.kontantstotte.innsyn.domain.IInnsynServiceClient;
+import no.nav.kontantstotte.innsyn.domain.InnsynOppslagException;
 import no.nav.security.oidc.OIDCConstants;
 import no.nav.security.oidc.test.support.JwtTokenGenerator;
 import no.nav.security.oidc.test.support.spring.TokenGeneratorConfiguration;
@@ -23,7 +25,6 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,11 +62,39 @@ public class BarnResourceTest {
         }});
 
         Response response = kallEndepunkt();
-
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+
         List<BarnDto> barnDtoList = response.readEntity(new GenericType<List<BarnDto>>() {});
-        barnDtoList.forEach(dto -> assertThat(dto.getFulltnavn()).isEqualTo(barn.getFulltnavn()));
-        barnDtoList.forEach(dto -> assertThat(dto.getFodselsdato()).isEqualTo(barn.getFodselsdato()));
+        assertThat(barnDtoList.get(0).getFulltnavn()).isEqualTo(barn.getFulltnavn());
+        assertThat(barnDtoList.get(0).getFodselsdato()).isEqualTo(barn.getFodselsdato());
+        assertThat(barnDtoList.get(0).getErFlerling()).isEqualTo(false);
+    }
+
+    @Test
+    public void at_uthenting_av_flerlinginformasjon_er_korrekt() {
+        Barn flerling1 = flerling1();
+        Barn flerling2 = flerling2();
+        when(innsynServiceMock.hentBarnInfo(any())).thenReturn(new ArrayList<Barn>() {{
+            add(flerling1);
+            add(flerling2);
+        }});
+
+        Response response = kallEndepunkt();
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+
+        List<BarnDto> barnDtoList = response.readEntity(new GenericType<List<BarnDto>>() {});
+
+        assertThat(barnDtoList.get(0).getFulltnavn()).isEqualTo(flerling1.getFulltnavn());
+        assertThat(barnDtoList.get(0).getFodselsdato()).isEqualTo(flerling1.getFodselsdato());
+        assertThat(barnDtoList.get(0).getErFlerling()).isEqualTo(false);
+
+        assertThat(barnDtoList.get(1).getFulltnavn()).isEqualTo(flerling2.getFulltnavn());
+        assertThat(barnDtoList.get(1).getFodselsdato()).isEqualTo(flerling2.getFodselsdato());
+        assertThat(barnDtoList.get(1).getErFlerling()).isEqualTo(false);
+
+        assertThat(barnDtoList.get(2).getFulltnavn()).isEqualTo("GUTTEBARN1 og GUTTEBARN2");
+        assertThat(barnDtoList.get(2).getFodselsdato()).isEqualTo(null);
+        assertThat(barnDtoList.get(2).getErFlerling()).isEqualTo(true);
     }
 
     @Test
@@ -83,7 +112,6 @@ public class BarnResourceTest {
     }
 
     private Response kallEndepunkt() {
-
         WebTarget target = ClientBuilder.newClient().register(LoggingFeature.class).target("http://localhost:" + port + contextPath);
         SignedJWT signedJWT = JwtTokenGenerator.createSignedJWT(INNLOGGET_BRUKER);
         return target.path("/barn")
@@ -97,10 +125,23 @@ public class BarnResourceTest {
 
     private Barn barn1() {
         return new Barn.Builder()
-                .fulltnavn("fornavn1 etternavn1")
-                .fodselsdato("11.11.1111")
+                .fulltnavn("NAVNESEN JENTEBARN")
+                .fodselsdato("01.01.2017")
                 .build();
     }
 
+    private Barn flerling1() {
+        return new Barn.Builder()
+                .fulltnavn("NAVNESEN GUTTEBARN1")
+                .fodselsdato("01.01.2018")
+                .build();
+    }
+
+    private Barn flerling2() {
+        return new Barn.Builder()
+                .fulltnavn("NAVNESEN GUTTEBARN2")
+                .fodselsdato("04.01.2018")
+                .build();
+    }
 }
 
