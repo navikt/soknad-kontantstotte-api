47dd163eb410cbe35fcf62e0eab66ed036b97084
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2018-12-20 14:51:06 +0100

    SD-373: Legg til støtte for flerlinger

--- src/main/java/no/nav/kontantstotte/api/rest/BarnResource.java
+++ src/main/java/no/nav/kontantstotte/api/rest/BarnResource.java
@@ -2,11 +2,8 @@ package no.nav.kontantstotte.api.rest;
 
 import no.nav.kontantstotte.api.rest.dto.BarnDto;
 import no.nav.kontantstotte.config.toggle.UnleashProvider;
-import no.nav.kontantstotte.innsyn.domain.Barn;
 import no.nav.kontantstotte.innsyn.domain.IInnsynServiceClient;
 import no.nav.security.oidc.api.ProtectedWithClaims;
-import no.nav.security.oidc.context.OIDCValidationContext;
-import no.nav.security.oidc.jaxrs.OidcRequestContext;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -14,6 +11,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,7 +28,7 @@ import static no.nav.kontantstotte.innlogging.InnloggingUtils.hentFnrFraToken;
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
 public class BarnResource {
 
-    private static final String SELVBETJENING = "selvbetjening";
+    private static final DateTimeFormatter DATO_PATTERN_SOKNAD = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private final IInnsynServiceClient innsynServiceClient;
 
@@ -41,16 +41,46 @@ public class BarnResource {
     public List<BarnDto> hentBarnInfoOmSoker() {
         String fnr = hentFnrFraToken();
         if(UnleashProvider.get().isEnabled(BRUK_TPS_INTEGRASJON)) {
-            List<Barn> sokerBarn = innsynServiceClient.hentBarnInfo(fnr);
-            return sokerBarn
+            List<BarnDto> sokerBarn = innsynServiceClient.hentBarnInfo(fnr)
                     .stream()
                     .map(barn -> new BarnDto(
                             barn.getFulltnavn(),
-                            barn.getFodselsdato()))
+                            barn.getFodselsdato(),
+                            false))
                     .collect(Collectors.toList());
+            sokerBarn.addAll(finnFlerlinger(sokerBarn));
+            return sokerBarn;
         } else {
             return new ArrayList<>();
         }
     }
 
+    private List<BarnDto> finnFlerlinger(List<BarnDto> barn) {
+        List<BarnDto> flerlinger = new ArrayList<>();
+        for (int i = 0; i < barn.size(); i++) {
+            for (int k = i + 1; k < barn.size(); k++) {
+                String fodselsdato1 = barn.get(i).getFodselsdato();
+                String fodselsdato2 = barn.get(k).getFodselsdato();
+                if (datoerInnenforIntervall(fodselsdato1, fodselsdato2)) {
+                    String kombinertNavn = kombinerNavn(barn.get(i).getFulltnavn(), barn.get(k).getFulltnavn());
+                    String kombinertFodselsdatoer = fodselsdato1.equals(fodselsdato2)
+                            ? fodselsdato1
+                            : fodselsdato1 + " og " + fodselsdato2;
+                    flerlinger.add(new BarnDto(kombinertNavn, kombinertFodselsdatoer, true));
+                }
+            }
+        }
+        return flerlinger;
+    }
+
+    private String kombinerNavn(String fulltnavn1, String fulltnavn2) {
+        String utenSlektsnavn1 = fulltnavn1.replaceAll(".*? ", "");
+        String utenSlektsnavn2 = fulltnavn2.replaceAll(".*? ", "");
+        return utenSlektsnavn1 + " og " + utenSlektsnavn2;
+    }
+
+    private boolean datoerInnenforIntervall(String dato1, String dato2) {
+        Period diff = Period.between(LocalDate.parse(dato1, DATO_PATTERN_SOKNAD), LocalDate.parse(dato2, DATO_PATTERN_SOKNAD));
+        return diff.getDays() <= 3;
+    }
 }
