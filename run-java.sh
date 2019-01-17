545a007700051a8f14fe20a8e0ef10cc20cade6d
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-01-17 12:21:28 +0100

    Test med dynamiske tiername og nodenames

--- run-java.sh
+++ run-java.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if test -r "/opt/appdynamics/javaagent.jar";
 then
+    export APPD_HOSTNAME="$FASIT_ENVIRONMENT_NAME-$HOSTNAME"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${FASIT_ENVIRONMENT_NAME}-${APP_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${FASIT_ENVIRONMENT_NAME}_${APP_NAME}_"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
