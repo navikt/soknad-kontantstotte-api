bab956c2c1f955793ecded9f1c3bd77a5ba13a92
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-02-18 15:59:50 +0100

    SD-766: Legg til mapping av landkode til land

--- src/main/java/no/nav/kontantstotte/api/rest/SokerResource.java
+++ src/main/java/no/nav/kontantstotte/api/rest/SokerResource.java
@@ -1,5 +1,7 @@
 package no.nav.kontantstotte.api.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
 import no.nav.kontantstotte.api.rest.dto.SokerDto;
@@ -7,13 +9,21 @@ import no.nav.kontantstotte.config.toggle.UnleashProvider;
 import no.nav.kontantstotte.innsyn.domain.InnsynService;
 import no.nav.kontantstotte.innsyn.domain.Person;
 import no.nav.security.oidc.api.ProtectedWithClaims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
 
 import static no.nav.kontantstotte.config.toggle.FeatureToggleConfig.BRUK_TPS_INTEGRASJON;
 import static no.nav.kontantstotte.innlogging.InnloggingUtils.hentFnrFraToken;
@@ -27,7 +37,11 @@ public class SokerResource {
 
     private final InnsynService innsynServiceClient;
 
+    private final Logger logger = LoggerFactory.getLogger(SokerResource.class);
+
     private final Counter soknadApnet = Metrics.counter("soknad.kontantstotte.apnet");
+
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Inject
     public SokerResource(InnsynService innsynServiceClient) {
@@ -40,10 +54,30 @@ public class SokerResource {
         if(UnleashProvider.get().isEnabled(BRUK_TPS_INTEGRASJON)) {
             Person person = innsynServiceClient.hentPersonInfo(fnr);
             soknadApnet.increment();
-            return new SokerDto(fnr, person.getFornavn(), person.getFulltnavn(), person.getStatsborgerskap());
+            return new SokerDto(fnr, person.getFornavn(), person.getFulltnavn(), kodeTilLand(person.getStatsborgerskap()));
         } else {
             return new SokerDto(fnr, null, null, null);
         }
     }
 
+    private String kodeTilLand(String landkode) {
+        String land = null;
+        try {
+            java.nio.file.Path currentRelativePath = Paths.get("");
+            String s = currentRelativePath.toAbsolutePath().toString();
+            logger.info("Current relative path is: " + s);
+            Map<String, String> landMap = mapper.readValue(
+                    SokerResource.class.getClassLoader().getResourceAsStream("tps/land-mapping.json"), new TypeReference<Map<String, String>>() {
+            });
+            land = landMap.get(landkode);
+        } catch (IOException e) {
+            logger.info(e.toString());
+            logger.info("Kunne ikke mappe land. Landkode " + landkode + " returnert.");
+        }
+        return land != null ? land : landkode;
+    }
+
+    private String getFile(String filnavn) {
+        return getClass().getClassLoader().getResource(filnavn).getFile();
+    }
 }
