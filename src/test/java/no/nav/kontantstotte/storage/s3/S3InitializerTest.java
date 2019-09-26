7d3bbd8f711f088485a2034824da8443916e4a62
Stig Strøm <stig.helge.strom@nav.no>
2019-09-26 08:52:22 +0200

    TestContainers kjører med jupiter (#173)

--- src/test/java/no/nav/kontantstotte/storage/s3/S3InitializerTest.java
+++ src/test/java/no/nav/kontantstotte/storage/s3/S3InitializerTest.java
@@ -2,25 +2,28 @@ package no.nav.kontantstotte.storage.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 
+@Testcontainers
+@DisabledIfEnvironmentVariable(named="CIRCLECI", matches="true")
 public class S3InitializerTest {
 
     private static final int SIZE_MB = 20;
 
-    @Rule
+    @Container
     public LocalStackContainer localStackContainer = new LocalStackContainer().withServices(S3);
 
     private AmazonS3 s3;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         s3 = AmazonS3ClientBuilder
                 .standard()
@@ -31,7 +34,6 @@ public class S3InitializerTest {
     }
 
     @Test
-    @DisabledIfEnvironmentVariable(named="CIRCLECI", matches="true")
     public void bucketIsCreatedwhenMissing() {
         assertThat(s3.listBuckets()).hasSize(0);
 
