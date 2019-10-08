d7fbf14f7eb633eafa92c5b6b76883361fd0e729
Martine Enger <marenger@student.matnat.uio.no>
2019-10-08 08:47:44 +0200

    Fjerner debuglog

--- src/main/java/no/nav/kontantstotte/innsending/oppsummering/OppsummeringPdfGenerator.java
+++ src/main/java/no/nav/kontantstotte/innsending/oppsummering/OppsummeringPdfGenerator.java
@@ -3,12 +3,10 @@ package no.nav.kontantstotte.innsending.oppsummering;
 import no.nav.familie.ks.kontrakter.søknad.Søknad;
 import no.nav.kontantstotte.innsending.Soknad;
 import no.nav.kontantstotte.innsending.oppsummering.html.OppsummeringHtmlGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class OppsummeringPdfGenerator {
 
-    private static final Logger LOG = LoggerFactory.getLogger(OppsummeringPdfGenerator.class);
     private final PdfConverter pdfConverter;
     private final OppsummeringHtmlGenerator oppsummeringHtmlGenerator;
 
@@ -23,7 +21,6 @@ public class OppsummeringPdfGenerator {
     }
     public byte[] genererNy(Søknad søknad, String fnr) {
         byte[] htmlBytes = oppsummeringHtmlGenerator.genererHtmlNy(søknad, fnr);
-        LOG.info("HTML: {}", new String(htmlBytes));
         return pdfConverter.genererPdf(htmlBytes);
     }
 }
