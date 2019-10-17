2fcb9cee670a4bbc2490996d4e585bcc83103819
Henning Håkonsen <henning.hakonsen@nav.no>
2019-10-17 09:24:02 +0100

    Fikser bean opprettelse. (#193)

--- src/main/java/no/nav/kontantstotte/client/RestClientConfiguration.java
+++ src/main/java/no/nav/kontantstotte/client/RestClientConfiguration.java
@@ -1,13 +1,12 @@
 package no.nav.kontantstotte.client;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RestClientConfiguration {
