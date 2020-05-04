9e67bae017271a284318e94ae77687a0b473e3aa
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-05-04 08:53:03 +0200

    Fikser skjult logging og håndtering av vedleggsfeil

--- src/main/java/no/nav/kontantstotte/config/ApiExceptionHandler.java
+++ src/main/java/no/nav/kontantstotte/config/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package no.nav.kontantstotte.config;
 
 import no.nav.kontantstotte.innsyn.domain.FortroligAdresseException;
+import no.nav.kontantstotte.storage.attachment.AttachmentConversionException;
 import no.nav.security.spring.oidc.validation.interceptor.OIDCUnauthorizedException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
@@ -9,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger secureLogger = LoggerFactory.getLogger("secureLogger");
-    private final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler({FortroligAdresseException.class})
     public ResponseEntity<String> handleFortroligAdresseException() {
@@ -30,15 +30,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Du er ikke logget inn");
     }
 
+    @ExceptionHandler({AttachmentConversionException.class})
+    public ResponseEntity<String> handleAttachmentConversionException(AttachmentConversionException e) {
+        logger.error("Kan ikke konvertere vedlegg : {}", ExceptionUtils.getStackTrace(e));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Feil ved mottak av vedlegg");
+    }
+
     @ExceptionHandler({Exception.class})
     public ResponseEntity<String> handleException(Exception e) {
         return error(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 
-
     private ResponseEntity<String> error(HttpStatus status, Exception e) {
         secureLogger.error("Exception : ", e);
-        logger.error("Exception : ", ExceptionUtils.getStackTrace(e));
+        logger.error("Exception : {}", ExceptionUtils.getStackTrace(e));
         return ResponseEntity.status(status).body("Feilen er logget vi ser på problemet!");
     }
 
