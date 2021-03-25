f9ac4f903b6f06f6a672c8a1637a383d84185855
Meng Zhu <meng.zhu@nav.no>
2021-03-25 10:57:20 +0100

    fix tests

--- src/main/java/no/nav/kontantstotte/dokument/DokumentService.java
+++ src/main/java/no/nav/kontantstotte/dokument/DokumentService.java
@@ -10,12 +10,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 import static no.nav.kontantstotte.innlogging.InnloggingUtils.hentFnrFraToken;
 
 @Component
 public class DokumentService {
+
     private static final Logger log = LoggerFactory.getLogger(DokumentService.class);
 
     private final Storage storage;
@@ -40,13 +44,47 @@ public class DokumentService {
             throw new IllegalArgumentException(HttpStatus.PAYLOAD_TOO_LARGE.toString());
         }
 
-        return familieDokumentClient.lagreVedlegg(multipartFile);
+        try {
+            return familieDokumentClient.lagreVedlegg(multipartFile);
+        }catch(Throwable e){
+            log.warn("Feil med å lagre vedlegg til familie-dokument");
+            Map<String, String> savedDokument = saveToStorage(multipartFile);
+            return savedDokument.get("vedleggsId");
+        }
     }
 
-    public byte[] hentDokument(String key){
-        byte[] dokument = familieDokumentClient.hentVedlegg(key);
-        if(dokument != null){
-            return dokument;
+    private Map<String, String> saveToStorage(MultipartFile multipartFile) throws IOException {
+
+        if (multipartFile.isEmpty()) {
+            return Map.of();
+        }
+
+        byte[] bytes = multipartFile.getBytes();
+        log.debug("Vedlegg med lastet opp med størrelse: " + bytes.length);
+
+        if (bytes.length > this.maxFileSize) {
+            throw new IllegalArgumentException(HttpStatus.PAYLOAD_TOO_LARGE.toString());
+        }
+
+        String directory = hentFnrFraToken();
+
+        String uuid = UUID.randomUUID().toString();
+
+        ByteArrayInputStream file = new ByteArrayInputStream(bytes);
+
+        storage.put(directory, uuid, file);
+
+        return Map.of("vedleggsId", uuid, "filnavn", multipartFile.getName());
+    }
+
+    public byte[] hentDokument(String key) {
+        try {
+            byte[] dokument = familieDokumentClient.hentVedlegg(key);
+            if (dokument != null) {
+                return dokument;
+            }
+        } catch (Throwable e) {
+            log.warn("Feil med hent av vedlegg fra familie-dokument");
         }
         String directory = hentFnrFraToken();
         byte[] data = storage.get(directory, key).orElse(null);
