08f2f0ae1810e26df923e4eb4cfc4d1e6ec36538
Meng Zhu <meng.zhu@nav.no>
2021-04-30 02:05:01 +0200

    add gcp profile

--- src/main/java/no/nav/kontantstotte/storage/attachment/AttachmentConfiguration.java
+++ src/main/java/no/nav/kontantstotte/storage/attachment/AttachmentConfiguration.java
@@ -2,9 +2,7 @@ package no.nav.kontantstotte.storage.attachment;
 
 import no.nav.kontantstotte.storage.encryption.EncryptedStorage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 import no.nav.kontantstotte.client.RestClientConfiguration;
 import no.nav.kontantstotte.storage.encryption.EncryptedStorageConfiguration;
@@ -18,10 +16,18 @@ public class AttachmentConfiguration {
         return new AttachmentToStorableFormatConverter(imageConversionService);
     }
 
+    @Profile("!gcp")
     @Bean
     public AttachmentStorage attachmentStorage(
             @Autowired EncryptedStorage storage,
             AttachmentToStorableFormatConverter storableFormatConverter) {
         return new AttachmentStorage(storage, storableFormatConverter);
+    }
+
+    @Profile("gcp")
+    @Primary
+    @Bean
+    public AttachmentStorage nonAttachmentStorage() {
+        return null;
     }
 }
