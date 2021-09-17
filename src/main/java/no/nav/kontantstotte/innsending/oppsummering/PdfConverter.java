1560edf2f61c6b91ef2a29918f6f736093fee7e6
Meng Zhu <meng.zhu@nav.no>
2021-09-17 10:59:42 +0200

    use familie-dokument for pdf generation

--- src/main/java/no/nav/kontantstotte/innsending/oppsummering/PdfConverter.java
+++ src/main/java/no/nav/kontantstotte/innsending/oppsummering/PdfConverter.java
@@ -1,23 +1,22 @@
 package no.nav.kontantstotte.innsending.oppsummering;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import no.nav.kontantstotte.client.HttpClientUtil;
-import no.nav.kontantstotte.client.TokenHelper;
 import no.nav.kontantstotte.innsending.InnsendingException;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
-import org.eclipse.jetty.http.HttpHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 @Component
 class PdfConverter {
@@ -26,40 +25,34 @@ class PdfConverter {
     private final HttpClient client;
     private URI pdfSvgSupportGeneratorUrl;
     private OIDCRequestContextHolder contextHolder;
+    private RestOperations restTemplate;
 
 
-    public PdfConverter(@Value("${SOKNAD_PDF_SVG_SUPPORT_GENERATOR_URL}") URI pdfSvgSupportGeneratorUrl,
-                        OIDCRequestContextHolder contextHolder) {
+    public PdfConverter(@Value("${FAMILIE_DOKUMENT_API_URL}") URI pdfSvgSupportGeneratorUrl,
+                        OIDCRequestContextHolder contextHolder,
+                        RestOperations restTemplate) {
         this.contextHolder = contextHolder;
         this.client = HttpClientUtil.create();
         this.pdfSvgSupportGeneratorUrl = pdfSvgSupportGeneratorUrl;
+        this.restTemplate = restTemplate;
     }
 
     byte[] genererPdf(byte[] bytes) {
+        HttpHeaders headers = new HttpHeaders();
+        URI dokumentUri = UriComponentsBuilder.fromUri(pdfSvgSupportGeneratorUrl).path("/html-til-pdf").build().toUri();
+        log.info("post uri {}", dokumentUri);
         try {
-            var request = HttpClientUtil.createRequest(TokenHelper.generateAuthorizationHeaderValueForLoggedInUser(contextHolder))
-                    .header(HttpHeader.CONTENT_TYPE.asString(), MediaType.TEXT_HTML_VALUE + "; charset=utf-8")
-                    .uri(URI.create(pdfSvgSupportGeneratorUrl + "v1/genpdf/html/kontantstotte"))
-                    .POST(HttpRequest.BodyPublishers.ofByteArray(bytes))
-                    .build();
-
-            var response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-
-            if (!HttpStatus.Series.SUCCESSFUL.equals(HttpStatus.Series.resolve(response.statusCode()))) {
-                throw new InnsendingException("Response fra pdf-generator: " + response.statusCode() + ". Response.entity: " + new String(response.body()));
-            }
-
-            log.info("Konvertert søknad til pdf");
-
-            return response.body();
-        } catch (JsonProcessingException e) {
-            throw new InnsendingException("Feiler under konvertering av innsending til json. " + e.getMessage());
-        } catch (InterruptedException e) {
-            throw new InnsendingException("Timer ut under innsending. " + e.getMessage());
-        } catch (IOException e) {
-            throw new InnsendingException("Ukjent IO feil. " + e.getMessage());
+            ResponseEntity<byte[]> response =
+                    restTemplate.exchange(dokumentUri,
+                                          HttpMethod.POST,
+                                          new HttpEntity<>(new String(bytes, StandardCharsets.UTF_8), headers),
+                                          byte[].class);
+            log.info("Generer Pdf med familie-dokument: {}", response.getStatusCode().toString());
+            return response.getBody();
+        } catch (Exception e) {
+            throw new InnsendingException("Feil med å genere Pdf med familie-dokument: "+ e.getMessage());
         }
     }
-
 }
+
 
