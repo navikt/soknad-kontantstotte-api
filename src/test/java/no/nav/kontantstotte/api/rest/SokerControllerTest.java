ac095c4dcd82fedd34ab3ec49f5dda6d8b4e356f
haldersaikat <saikat.halder@nav.no>
2022-02-11 12:11:06 +0100

    fikset review kommentar og fjernet gamle propertier

--- src/test/java/no/nav/kontantstotte/api/rest/SokerControllerTest.java
+++ src/test/java/no/nav/kontantstotte/api/rest/SokerControllerTest.java
@@ -75,7 +75,7 @@ public class SokerControllerTest {
     }
 
     @Test
-    public void at_tps_feil_gir_500() {
+    public void at_pdl_feil_gir_500() {
         when(innsynServiceMock.hentPersonInfo(any())).thenThrow(new InnsynOppslagException("Feil i pdl"));
         Response response = kallEndepunkt();
         assertThat(response.getStatus()).isEqualTo(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
@@ -89,7 +89,7 @@ public class SokerControllerTest {
     }
 
     @Test
-    public void at_tps_feil_legger_pa_cors_filter() {
+    public void at_pdl_feil_legger_pa_cors_filter() {
         when(innsynServiceMock.hentPersonInfo(any())).thenThrow(new InnsynOppslagException("Feil i pdl"));
         Response response = kallEndepunkt();
         assertThat(response.getHeaders()).containsKey("Access-Control-Allow-Origin");
